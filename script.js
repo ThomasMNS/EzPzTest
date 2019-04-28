a4b2791966d344cc86e0206c73ab56ae15b2f779
ThomasMNS <thomasmns@outlook.com>
2019-04-28 22:21:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+var request = new XMLHttpRequest();
 
+request.open('GET', 'http://thomasmns.000webhostapp.com/read.php');
+
+request.onload = function () {
+  var data = JSON.parse(this.response);
+  console.log(data);
+  }
+}
+
+request.send();
