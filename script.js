b43a4b94e8d236b7e6205bd19125ce0ada2b4ccd
ThomasMNS <thomasmns@outlook.com>
2019-04-28 22:29:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ request.open('GET', 'http://thomasmns.000webhostapp.com/read.php');
 request.onload = function () {
   var data = JSON.parse(this.response);
   console.log(data);
-  console.log("THIS HAS HAPPENED");
-}
+  console.log('THIS HAS HAPPENED');
+};
 
 request.send();
 
-console.log("THING");
+console.log('THING');
